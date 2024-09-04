d923ed31c74fb67094b93b220ecf0ba0d27f9430
Anselmo Gomes <contato.anselmo@gmail.com>
2024-09-04 10:16:49 -0300

    INIT: povoamento do repositório

---
+++ README.md
@@ -0,0 +1,31 @@
+<h1 align="center">Gigabaite Tecnologia</h1>
+<h1 align="center">WhaTicket by Gigabaite</h1> 
+<h1 align="center">Sistema de tickets completo baseado em mensagens do WhatsApp</h1> 
+
+
+## Instruções de Instalação
+
+Primeira instalação:
+
+```bash
+sudo apt install -y git && git clone https://github.com/oanselmogomes/stp install && sudo chmod -R 777 ./install && cd ./install && sudo ./install_primaria
+```
+
+Instalação de instância:
+
+```bash
+cd && cd ./install && sudo ./install_instancia
+```
+
+## Requisitos do Sistema
+
+- VPS Ubuntu 20.04 com 3 vCPU's + 4 GB RAM)
+- Subdominio para Frontend
+- Subdominio para API
+- E-mail válido para certificação SSL
+
+## Suporte Premium e Personalizações
+
+    Gigabaite Tecnologia
+
+    https://www.gigabaite.com.br
---
+++ lib/_inquiry.sh
@@ -0,0 +1,244 @@
+#!/bin/bash
+
+get_mysql_root_password() {
+  
+  print_banner
+  printf "${WHITE} 💻 Insira senha para o usuario Deploy e Banco de Dados (não utilizar caracteres especiais):${GRAY_LIGHT}"
+  printf "\n\n"
+  read -p "> " mysql_root_password
+}
+
+get_link_git() {
+  
+  print_banner
+  printf "${WHITE} 💻 Insira o link do GitHub do seu WhaTicket by Gigabaite que deseja instalar:${GRAY_LIGHT}"
+  printf "\n\n"
+  read -p "> " link_git
+}
+
+get_instancia_add() {
+  
+  print_banner
+  printf "${WHITE} 💻 Informe um nome para a instancia que será instalada (não utilizar espaços ou caracteres especiais, utilizar letras minusculas; ):${GRAY_LIGHT}"
+  printf "\n\n"
+  read -p "> " instancia_add
+}
+
+get_max_whats() {
+  
+  print_banner
+  printf "${WHITE} 💻 Informe a Qtde de Conexões/Whats que a ${instancia_add} poderá cadastrar:${GRAY_LIGHT}"
+  printf "\n\n"
+  read -p "> " max_whats
+}
+
+get_max_user() {
+  
+  print_banner
+  printf "${WHITE} 💻 Informe a Qtde de Usuarios/Atendentes que a ${instancia_add} poderá cadastrar:${GRAY_LIGHT}"
+  printf "\n\n"
+  read -p "> " max_user
+}
+
+get_frontend_url() {
+  
+  print_banner
+  printf "${WHITE} 💻 Digite o domínio do Frontend para a ${instancia_add}:${GRAY_LIGHT}"
+  printf "\n\n"
+  read -p "> " frontend_url
+}
+
+get_backend_url() {
+  
+  print_banner
+  printf "${WHITE} 💻 Digite o domínio do Backend para a ${instancia_add}:${GRAY_LIGHT}"
+  printf "\n\n"
+  read -p "> " backend_url
+}
+
+get_frontend_port() {
+  
+  print_banner
+  printf "${WHITE} 💻 Digite a porta do Frontend para a ${instancia_add}; Ex: 3000 A 3999 ${GRAY_LIGHT}"
+  printf "\n\n"
+  read -p "> " frontend_port
+}
+
+
+get_backend_port() {
+  
+  print_banner
+  printf "${WHITE} 💻 Digite a porta do Backend para esta instancia; Ex: 4000 A 4999 ${GRAY_LIGHT}"
+  printf "\n\n"
+  read -p "> " backend_port
+}
+
+get_redis_port() {
+  
+  print_banner
+  printf "${WHITE} 💻 Digite a porta do Redis para a ${instancia_add}; Ex: 5000 A 5999 ${GRAY_LIGHT}"
+  printf "\n\n"
+  read -p "> " redis_port
+}
+
+get_empresa_delete() {
+  
+  print_banner
+  printf "${WHITE} 💻 Digite o nome da instancia que será removida (digite o mesmo nome de quando instalou):${GRAY_LIGHT}"
+  printf "\n\n"
+  read -p "> " empresa_delete
+}
+
+get_empresa_atualizar() {
+  
+  print_banner
+  printf "${WHITE} 💻 Digite o nome da instancia que será atualizada (digite o mesmo nome de quando instalou):${GRAY_LIGHT}"
+  printf "\n\n"
+  read -p "> " empresa_atualizar
+}
+
+get_empresa_bloquear() {
+  
+  print_banner
+  printf "${WHITE} 💻 Digite o nome da instancia que será bloqueada (digite o mesmo nome de quando instalou):${GRAY_LIGHT}"
+  printf "\n\n"
+  read -p "> " empresa_bloquear
+}
+
+get_empresa_desbloquear() {
+  
+  print_banner
+  printf "${WHITE} 💻 Digite o nome da instancia que será desbloqueada (digite o mesmo nome de quando instalou):${GRAY_LIGHT}"
+  printf "\n\n"
+  read -p "> " empresa_desbloquear
+}
+
+get_empresa_dominio() {
+  
+  print_banner
+  printf "${WHITE} 💻 Digite o nome da instancia que deseja alterar os dominios (atenção para alterar os dominios precisa digitar os 2, mesmo que vá alterar apenas 1):${GRAY_LIGHT}"
+  printf "\n\n"
+  read -p "> " empresa_dominio
+}
+
+get_alter_frontend_url() {
+  
+  print_banner
+  printf "${WHITE} 💻 Digite o novo domínio do Frontend para a ${empresa_dominio}:${GRAY_LIGHT}"
+  printf "\n\n"
+  read -p "> " alter_frontend_url
+}
+
+get_alter_backend_url() {
+  
+  print_banner
+  printf "${WHITE} 💻 Digite o novo domínio do Backend para a ${empresa_dominio}:${GRAY_LIGHT}"
+  printf "\n\n"
+  read -p "> " alter_backend_url
+}
+
+get_alter_frontend_port() {
+  
+  print_banner
+  printf "${WHITE} 💻 Digite a porta do Frontend da instancia ${empresa_dominio}; a porta deve ser o mesma informada durante a instalação ${GRAY_LIGHT}"
+  printf "\n\n"
+  read -p "> " alter_frontend_port
+}
+
+
+get_alter_backend_port() {
+  
+  print_banner
+  printf "${WHITE} 💻 Digite a porta do Backend da instancia ${empresa_dominio}; a porta deve ser o mesma informada durante a instalação ${GRAY_LIGHT}"
+  printf "\n\n"
+  read -p "> " alter_backend_port
+}
+
+
+get_urls() {
+  get_mysql_root_password
+  get_link_git
+  get_instancia_add
+  get_max_whats
+  get_max_user
+  get_frontend_url
+  get_backend_url
+  get_frontend_port
+  get_backend_port
+  get_redis_port
+}
+
+software_update() {
+  get_empresa_atualizar
+  frontend_update
+  backend_update
+}
+
+software_delete() {
+  get_empresa_delete
+  deletar_tudo
+}
+
+software_bloquear() {
+  get_empresa_bloquear
+  configurar_bloqueio
+}
+
+software_desbloquear() {
+  get_empresa_desbloquear
+  configurar_desbloqueio
+}
+
+software_dominio() {
+  get_empresa_dominio
+  get_alter_frontend_url
+  get_alter_backend_url
+  get_alter_frontend_port
+  get_alter_backend_port
+  configurar_dominio
+}
+
+inquiry_options() {
+  
+  print_banner
+  printf "${WHITE} 💻 Gerenciador do WhaTicket by Gigabaite. Selecione uma ação abaixo:${GRAY_LIGHT}"
+  printf "\n\n"
+  printf "   [0] Instalar\n"
+  printf "   [1] Atualizar\n"
+  printf "   [2] Deletar\n"
+  printf "   [3] Bloquear\n"
+  printf "   [4] Desbloquear\n"
+  printf "   [5] Alterar Dominio\n"
+  printf "\n"
+  read -p "> " option
+
+  case "${option}" in
+    0) get_urls ;;
+
+    1) 
+      software_update 
+      exit
+      ;;
+
+    2) 
+      software_delete 
+      exit
+      ;;
+    3) 
+      software_bloquear 
+      exit
+      ;;
+    4) 
+      software_desbloquear 
+      exit
+      ;;
+    5) 
+      software_dominio 
+      exit
+      ;;        
+
+    *) exit ;;
+  esac
+}
+
+
